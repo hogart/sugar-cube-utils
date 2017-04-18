c8dcfe67841d2f6cfddd8a83c2eadd05d891e1ee
Konstantin Kitmanov <doctor.hogart@gmail.com>
2017-04-18 23:56:29 +0200

    make abbr macro use current text colors + remove obsolete `insertElement`

--- abbr.js
+++ abbr.js
@@ -1,7 +1,7 @@
 (function() {
     // usage: <<abbr "text" "long explanation">>
     'use strict';
-    /* globals version, Macro, jQuery, insertElement */
+    /* globals version, Macro, jQuery */
 
     if (!version || !version.title || 'SugarCube' !== version.title || !version.major || version.major < 2) {
         throw new Error('<<abbr>> macro requires SugarCube 2.0 or greater, aborting load');
@@ -29,8 +29,7 @@
             font-size: 90%;
             pointer-events: none;
             opacity: 0;
-            border: 1px solid;
-            border-color: #444;
+            border: 1px solid currentColor;
             transition: 150ms linear all; 
         }
         .${clsPrefix}:active::before,
@@ -39,26 +38,17 @@
             opacity: 1;
             transition: 150ms linear all; 
         }
-        .${clsPrefix}::before {
-            color: #eee;
-            background-color: #111;
-        }
-        html.daymode .${clsPrefix}::before { /* remove this if you're not using daymode */
-            color: #111;
-            background-color: #fff;
-            border-color: #ccc;
-            transition: 150ms linear all; 
+        /* to avoid setting bg color manually */
+        #story, #passages, .passage, .${clsPrefix}, .${clsPrefix}::before {
+            background-color: inherit;
         }`;
 
     jQuery('head').append(`<style type="text/css">${styles}</style>`);
 
     Macro.add('abbr', {
         handler () {
-            const abbr = insertElement(this.output, 'abbr');
-            abbr.classList.add(clsPrefix);
-
-            abbr.innerHTML = this.args[0];
-            abbr.dataset.title = this.args[1];
+            const abr = jQuery(`<abbr class="${clsPrefix}" data-title="${this.args[1]}">${this.args[0]}</abbr>`)
+            abr.appendTo(this.output);
         }
     });
 }());
