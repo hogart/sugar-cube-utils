f1b6264a1e50d1e4722acaa4cafa518bc1dce076
Konstantin Kitmanov <doctor.hogart@gmail.com>
2017-04-22 16:46:56 +0200

    improve qbn

--- qbn.js
+++ qbn.js
@@ -5,31 +5,61 @@
      * Simple quality-based quest tracker. Usage:
      *
      * qbn.set('house', 'ground floor');
-     * qbn.set('house', 'basement');
-     * if (qbn.length('dungeon') === 2) { alert('house fully explored') }
+     * qbn.set('house', ['basement']);
+     * if (qbn.length('house') === 2) { alert('house fully explored') }
      * qbn.set('house', 'ground floor'); // qbn.length('dungeon') still equals 2
+     * qbn.unset('house', 'ground floor');
      */
 
     const qualities = new Map();
     const increments = new Map();
 
+    function addValuesToSet(set, values) {
+        values.forEach((value) => set.add(value))
+    }
+
     window.qbn = {
-        set(qualityName, value) {
-            const set = qualities.get(qualityName);
+        set(qualityName, values) {
+            let set = qualities.get(qualityName);
             if (!set) {
-                qualities.set(qualityName, new Set());
+                set = new Set();
+                qualities.set(qualityName, set);
             }
 
-            qualities.get(qualityName).add(value);
+            if (Array.isArray(values)) {
+                addValuesToSet(set, values);
+            } else {
+                set.add(values);
+            }
         },
 
-        unset(qualityName, value) {
+        unset(qualityName, values) {
             const set = qualities.get(qualityName);
 
             if (set) {
-                return set.has(value);
-            } else {
+                if (Array.isArray(values)) {
+                    values.forEach((value) => set.delete(value));
+                } else {
+                    set.delete(values);
+                }
+            }
+        },
+
+        has(qualityName, values) {
+            const set = qualities.get(qualityName);
+            if (!set) {
                 return false;
+            } else {
+                if (Array.isArray(values)) {
+                    for (const value of values.values()) {
+                        if (!set.has(value)) {
+                            return false;
+                        }
+                    }
+                    return true;
+                } else {
+                    return set.has(values);
+                }
             }
         },
 
