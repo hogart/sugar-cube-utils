8c5fffcf8c3232876033fcafb01781c94225f148
Konstantin Kitmanov <doctor.hogart@gmail.com>
2017-12-26 15:10:30 +0100

    add menuButton utility, expose as window.scUtils.* functions

---
+++ menuButton.js
@@ -0,0 +1,86 @@
+(function () {
+    'use strict';
+
+    // Utility functions to create buttons in dock menu.
+    // scUtils.createPassageButton creates button which opens dialog displaying passage with given name.
+    // scUtils.createHandlerButton creates button which calls given handler.
+    // Both methods return {button, style} objects with jQuery-wrapped references to created elements
+
+    /* globals Story, Dialog, jQuery */
+
+    // save some DOM references for later use
+    const $head = jQuery('head');
+    const $menuCore = jQuery('#menu-core');
+
+    function createButton(id, label, onClick) {
+        const buttonTemplate = `<li id="${id}"><a>${label}</a></li>`;
+        const $button = jQuery(buttonTemplate);
+
+        $button.ariaClick(onClick);
+        $button.appendTo($menuCore);
+
+        return $button;
+    }
+
+    function createButtonStyle(id, iconContent) {
+        const styles = `
+            #menu-core #${id} a::before {
+                ${iconContent ? `content: '${iconContent}'` : ''};
+            }
+        `;
+
+        const $style = jQuery(`<style type="text/css" id="${id}-style">${styles}</style>`);
+        $style.appendTo($head);
+
+        return $style;
+    }
+
+    function createDlgFromPassage(passageName, title) {
+        const content = Story.get(passageName).processText();
+
+        Dialog.setup(title);
+        Dialog.wiki(content);
+        Dialog.open();
+    }
+
+    /**
+     * Creates button in UI dock opening given passage.
+     * @param {string} label Button label
+     * @param {string} iconContent Some UTF sequence, like `\\e809\\00a0`
+     * @param {string} passageName Passage name to display in dialogue
+     * @return {{button: jQuery, style: jQuery}}
+     */
+    function createPassageButton(label, iconContent, passageName) {
+        const id = `menu-item-${passageName}`;
+
+        return {
+            button: createButton(id, label, () => createDlgFromPassage(passageName, label)),
+            style: createButtonStyle(id, iconContent),
+        };
+    }
+
+    /**
+     * Creates button in UI dock which calls `handler` when clicked.
+     * @param {string} label Button label
+     * @param {string} iconContent Some UTF sequence, like `\e809\00a0`
+     * @param {string} shortName any unique identifier, only letters, digits, dashes, underscore
+     * @param {Function} handler Function to call on click/tap
+     * @return {{button: jQuery, style: jQuery}}
+     */
+    function createHandlerButton(label, iconContent, shortName, handler) {
+        const id = `menu-item-${shortName}`;
+
+        return {
+            button: createButton(id, label, handler),
+            style: createButtonStyle(id, iconContent),
+        };
+    }
+
+    window.scUtils = Object.assign(
+        window.scUtils || {},
+        {
+            createPassageButton,
+            createHandlerButton,
+        }
+    );
+}());
