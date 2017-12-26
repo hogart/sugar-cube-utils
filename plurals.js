01c6c93e27fbffdcc9cab244b21adbf506605f55
Konstantin Kitmanov <doctor.hogart@gmail.com>
2017-12-26 15:14:50 +0100

    simple pluralizer, relies on Intl.PluralRules present or polyfilled

---
+++ plurals.js
@@ -0,0 +1,62 @@
+(function () {
+    'use strict';
+
+    const localeTag = document.documentElement.getAttribute('lang') || 'en';
+    const pluralRules = new Intl.PluralRules(localeTag);
+    const indices = {
+        ru: {
+            one: 0, // singular
+            many: 1, // plural
+            few: 2, // 2-4
+        },
+
+        en: {
+            one: 0, // singular
+            other: 1, // plural or zero
+        },
+
+        // TODO: add more languages
+    };
+
+    // TODO: add more slavic languages here
+    indices.pl = indices.ua = indices.ru; // slavic languages share singular/paucal/plural rules
+
+    const langIndex = indices[localeTag] || indices.en;
+
+    /**
+     *
+     * @param {number} amount
+     * @param {string[]} cases For English: ['cat', 'cats]. For slavic languages: ['яблоко', 'яблок', 'яблока']
+     * @returns {string}
+     */
+    function pluralize(amount, cases) {
+        const rule = pluralRules.select(amount);
+        const index = langIndex[rule];
+
+        return cases[index];
+    }
+
+    const amountRe = /\${amount}/mg;
+    const pluralRe = /\${plural}/mg;
+
+    function pluralizeFmt(cases, tpl) {
+        function fmt(amount, plural) {
+            return tpl
+                .replace(amountRe, amount)
+                .replace(pluralRe, plural);
+        }
+
+        return (amount) => {
+            const plural = pluralize(amount, cases);
+            return fmt(amount, plural);
+        }
+    }
+
+    window.scUtils = Object.assign(
+        window.scUtils || {},
+        {
+            pluralize,
+            pluralizeFmt,
+        }
+    );
+}());
