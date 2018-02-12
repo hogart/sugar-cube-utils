a1c3e012761154f6ff11b74ca336c4071ea634c8
Konstantin Kitmanov <doctor.hogart@gmail.com>
2018-02-13 00:12:01 +0100

    make checkbox macro also accept labels for on and off states

--- iconcheckbox.js
+++ iconcheckbox.js
@@ -1,5 +1,7 @@
 (function() {
-    // usage: <<iconcheck $isSomething>><</iconcheck>>
+    // usage:
+    //  <<iconcheck $isSomething>>toggle value<</iconcheck>>
+    // <<iconcheck $isSomething "Turn on" "Turn off">><</iconcheck>>
     'use strict';
     /* globals version, Macro, jQuery, Wikifier, State */
 
@@ -31,27 +33,37 @@
     Macro.add('iconcheck', {
         tags: null,
         handler () {
+            const {args, payload} = this;
             // Ensure that the variable name argument is a string.
-            if (typeof this.args[0] !== 'string') {
+            if (typeof args[0] !== 'string') {
                 return this.error('variable name argument is not a string');
             }
 
-            let varName = this.args[0].trim();
+            let varName = args[0].trim();
 
             // Try to ensure that we receive the variable's name (incl. sigil), not its value.
             if (varName[0] !== '$' && varName[0] !== '_') {
-                return this.error(`variable name "${this.args[0]}" is missing its sigil ($ or _)`);
+                return this.error(`variable name "${args[0]}" is missing its sigil ($ or _)`);
             }
 
             varName = varName.substring(1);
 
+            const $span = jQuery('<span></span>');
             const $input = jQuery(`<input type="checkbox" ${State.variables[varName] ? 'checked' : ''}/>`);
             $input.on('change', () => {
                 State.variables[varName] = $input.prop('checked');
+                $span.html(getLabel());
             });
 
-            const $span = jQuery('<span></span>');
-            new Wikifier($span, this.payload[0].contents);
+            function getLabel() {
+                if (args.length === 3) {
+                    return State.variables[varName] ? args[1] : args[2];
+                } else {
+                    return payload[0].contents;
+                }
+            }
+
+            new Wikifier($span, getLabel());
 
             const $label = jQuery(`<label class="${clsPrefix}"></label>`);
 
