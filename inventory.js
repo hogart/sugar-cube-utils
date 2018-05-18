9a36aaab8c5529ff5ba81c3e2a3281545bc65f5c
Konstantin Kitmanov <doctor.hogart@gmail.com>
2018-05-19 01:44:02 +0200

    onPassagePresent util function

--- inventory.js
+++ inventory.js
@@ -2,13 +2,13 @@
     'use strict';
 
     /* globals jQuery, Engine */
+    // requires onPassagePresent.js
 
     // Utility functions to manage inventory appearance.
     // Inventory is displayed as PassageHeader (or PassageFooter) and consists of button that opens
     // some kind of overlay. This button should have `js-toggleInventory` class. Overlay should have
     // `js-inventory` class and open by assigning `open` class.
 
-    const $doc = jQuery(document);
     const $body = jQuery('body');
 
     $body.on('click', '.js-toggleInventory', function (event) {
@@ -25,13 +25,8 @@
     }
 
     function hiliteInventory(className = 'ping-inventory', timeout = 3500) {
-        if (Engine.isRendering()) { // if we're calling this fn from <<run>> or <<script>> directly, when passage is prepared
-            $doc.one(':passagedisplay', () => pingInventory(className, timeout));
-        } else { // if it's called from inside <<linkappend>> or something, after passage've been displayed already
-            pingInventory(className, timeout);
-        }
+        window.scUtils.onPassagePresent(() => pingInventory(className, timeout));
     }
-
 
     window.scUtils = Object.assign(
         window.scUtils || {},
---
+++ onPassagePresent.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    /* globals jQuery, Engine */
+
+    const $doc = jQuery(document);
+
+    function onPassagePresent(callback) {
+        if (Engine.isRendering()) { // if we're calling this fn from <<run>> or <<script>> directly, when passage is prepared
+            $doc.one(':passagedisplay', callback);
+        } else { // if it's called from inside <<linkappend>> or something, after passage've been displayed already
+            callback();
+        }
+    }
+
+    window.scUtils = Object.assign(
+        window.scUtils || {},
+        {
+            onPassagePresent
+        },
+    );
+}());
