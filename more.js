550b78caca6ba6cc2de12ea0a446b12fc1cad20e
Konstantin Kitmanov <doctor.hogart@gmail.com>
2018-11-30 23:59:40 +0100

    better tooltips for <<more>>

--- more.js
+++ more.js
@@ -1,8 +1,7 @@
 (function() {
-    // container version of <<abbr>>
     // usage: <<more "tooltip">>content<</more>>
     'use strict';
-    /* globals version, Macro, jQuery, Wikifier */
+    /* globals version, Macro, jQuery */
 
     if (!version || !version.title || 'SugarCube' !== version.title || !version.major || version.major < 2) {
         throw new Error('<<more>> macro requires SugarCube 2.0 or greater, aborting load');
@@ -10,48 +9,148 @@
 
     version.extensions.more = {major: 1, minor: 0, revision: 0};
 
-    const clsPrefix = 'more-macro';
+    'use strict';
+
+    const clsPrefix = 'moremacro';
+
+    class Tooltiper {
+        constructor(options) {
+            this.options = Object.assign({}, {
+                container: document.body,
+                clsPrefix: 'moremacro',
+            }, options);
+
+            this.container = typeof this.options.container === 'string' ? document.querySelector(this.options.container) : this.options.container;
+
+            this.target = null;
+
+            this.onMouseOver = this.onMouseOver.bind(this);
+            this.onMouseOut = this.onMouseOut.bind(this);
+
+            this.addListeners();
+        }
+
+        createTooltip() {
+            this.el = document.createElement('div');
+            this.el.className = this.options.clsPrefix;
+            this.container.appendChild(this.el);
+        }
+
+        onMouseOver(event) {
+            const target = event.target.closest(`[data-${this.options.clsPrefix}]`);
+
+            if (target) {
+                if (this.target !== target) {
+                    this.target = target;
+                }
+
+                this.show();
+            } else {
+                this.hide();
+            }
+        }
+
+        onMouseOut(event) {
+            if (event.relatedTarget === this.el) {
+                return;
+            }
+
+            this.hide();
+        }
+
+        show() {
+            if (!this.el) {
+                this.createTooltip();
+            }
+
+            const targetRect = this.target.getBoundingClientRect();
+
+            this.el.innerHTML = this.target.dataset[clsPrefix];
+
+            const position = this.getPosition(targetRect);
+
+            this.el.style.minWidth = targetRect.width + 'px';
+            this.el.style.top = position.top + pageYOffset + targetRect.height + 'px';
+            this.el.style.left = position.left + pageXOffset + 'px';
+
+            this.el.classList.add('visible');
+        }
+
+        getPosition(rect) {
+            let {top, left} = rect;
+            const {offsetHeight, offsetWidth} = this.el;
+
+            if (rect.top + rect.height + offsetHeight > window.innerHeight) {
+                top = rect.top - (offsetHeight + rect.height);
+            }
+
+            if (rect.left + rect.width + offsetWidth > window.innerWidth) {
+                left = rect.left - Math.abs(rect.width - offsetWidth);
+
+                if (left < 0) {
+                    left = 0;
+                }
+            }
+
+            return {top, left};
+        }
+
+        hide() {
+            if (this.el) {
+                this.el.classList.remove('visible');
+            }
+        }
+
+        addListeners() {
+            this.container.addEventListener('mouseover', this.onMouseOver);
+            this.container.addEventListener('mouseout', this.onMouseOut);
+        }
+
+        removeListeners() {
+            this.container.removeEventListener('mouseover', this.onMouseOver);
+            this.container.removeEventListener('mouseout', this.onMouseOut);
+        }
+
+        destroy() {
+            this.removeListeners();
+        }
+    }
 
     const styles = `
         .${clsPrefix} {
-            position: relative;
-            display: inline;
-            cursor: pointer;
-            border-bottom: 1px dotted;
-        }
-        .${clsPrefix}::before {
-            content: attr(data-title);
             position: absolute;
-            display: table;
-            top: 100%;
-            left: 0;
-            z-index: 10;
-            max-width: 25vw;
-            padding: 0.3em;
-            font-size: 90%;
-            pointer-events: none;
+            z-index: 1000;
             opacity: 0;
-            border: 1px solid currentColor;
-            transition: 150ms linear all; 
-        }
-        .${clsPrefix}:active::before,
-        .${clsPrefix}:hover::before {
-            pointer-events: auto;
-            opacity: 1;
-            transition: 150ms linear all; 
-        }
-        /* to avoid setting bg color manually */
-        #story, #passages, .passage, .passage *, .passage * .${clsPrefix}, .${clsPrefix}::before {
             background-color: inherit;
-        }`;
+            border: 1px solid currentColor;
+            box-shadow: 0 1ex 1ex 2px;
+            max-width: 90%;
+            padding: 4px 10px;
+            font-size: 90%;
+            transition: opacity 150ms linear;
+            pointer-events: auto;
+        }
+        
+        .${clsPrefix}.visible {
+            opacity: 1;
+            pointer-events: none;
+        }
+        
+        [data-${clsPrefix}] {
+            border-bottom: 1px dotted currentColor;
+            cursor: pointer;
+        }
+        `;
 
     jQuery('head').append(`<style type="text/css">${styles}</style>`);
+
+    new Tooltiper({container: '#story'});
 
     Macro.add('more', {
         tags: null,
         handler () {
-            const more = jQuery(`<abbr class="${clsPrefix}" data-title="${this.args[0]}"></abbr>`);
-            new Wikifier(more, this.payload[0].contents);
+            const more = jQuery(`<abbr data-${clsPrefix}="${this.args[0]}"></abbr>`);
+            more.wiki(this.payload[0].contents);
             more.appendTo(this.output);
         },
     });
