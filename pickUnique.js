f6144f7a40e4d2401520f6667ad69fd8916a5bd0
Konstantin Kitmanov <konstantin.kitmanov@fabres.pl>
2019-04-09 13:42:59 +0200

    advanced unique picking functions

--- pickUnique.js
+++ pickUnique.js
@@ -1,36 +1,88 @@
 (function () {
     'use strict';
 
-    /* globals random */
+    /* globals random, Story */
 
     const lastIndex = '__lastIndex';
 
+    function _extendArray(arr, value = null) {
+        if (value === null) {
+            value = random(arr.length - 1)
+        }
+
+        Object.defineProperty(arr, lastIndex, {
+            enumerable: false,
+            writable: true,
+            value,
+        });
+    }
+
+    function _getNextUnique(arr) {
+        let newValue;
+
+        do {
+            newValue = random(arr.length - 1);
+        } while (newValue === arr[lastIndex]);
+
+        arr[lastIndex] = newValue;
+        return arr[arr[lastIndex]];
+    }
+
+    /**
+     * @param {any[]} arr
+     * @return {any}
+     */
     function pickUnique(arr) {
         if (!arr.hasOwnProperty(lastIndex)) {
             const value = random(arr.length - 1);
-            Object.defineProperty(arr, lastIndex, {
-                enumerable: false,
-                writable: true,
-                value,
-            });
+            _extendArray(arr, value);
 
             return arr[value];
         } else {
-            let newValue;
-
-            do {
-                newValue = random(arr.length - 1);
-            } while (newValue === arr[lastIndex]);
-
-            arr[lastIndex] = newValue;
-            return arr[arr[lastIndex]];
+            return _getNextUnique(arr);
         }
+    }
+
+    function createUniquePicker(arr) {
+        _extendArray(arr);
+
+        return function () {
+            return _getNextUnique(arr);
+        }
+    }
+
+    function _getPassageLines(passage) {
+        let text;
+        if (passage.processText) { // we have passage itself
+            text = passage.processText();
+        } else { // we have passage name
+            const rawPassage = Story.get(passage);
+            if (!rawPassage) {
+                throw new Error(`No such passage: "${passage}".`)
+            }
+
+            text = rawPassage.processText();
+        }
+
+        return text.split('\n');
+    }
+
+    /**
+     * @param {string|Passage} passage
+     * @return {function (): string}
+     */
+    function createUniquePickerFromPassage(passage) {
+        const arr = _getPassageLines(passage);
+
+        return createUniquePicker(arr);
     }
 
     window.scUtils = Object.assign(
         window.scUtils || {},
         {
             pickUnique,
+            createUniquePicker,
+            createUniquePickerFromPassage,
         }
     );
 }());
