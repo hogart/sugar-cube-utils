7571bd7ec789f5e7ed87abbb1fe099e4775b2df7
Konstantin Kitmanov <doctor.hogart@gmail.com>
2019-09-25 00:59:02 +0200

    preloadImages utility

---
+++ preloadImages.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+
+    /* globals LoadScreen */
+
+    function preloadImage(source, strict = false) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => {
+                resolve(img);
+            };
+            if (strict) {
+                img.onerror = reject;
+            } else {
+                img.onerror = () => {
+                    resolve(null);
+                };
+            }
+
+            img.src = source;
+        });
+    }
+
+    function preloadImages(sources, strict) {
+        const lock = LoadScreen.lock();
+        const method = strict ? Promise.all : Promise.any;
+        return method(
+            sources.map(
+                (src) => preloadImage(src, strict)
+            )
+        ).finally(() => LoadScreen.unlock(lock));
+    }
+
+    window.scUtils = Object.assign(
+        window.scUtils || {},
+        {
+            preloadImages,
+        }
+    );
+}());
